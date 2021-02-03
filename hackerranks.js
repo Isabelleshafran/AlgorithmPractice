02fc54e00b20262439ea91336a6c16e6ac51a97a
ishafran <isabelle.shafran@gmail.com>
2021-02-03 14:30:00 -0800

    hackerranks

--- hackerranks.js
+++ hackerranks.js
@@ -1,3 +1,53 @@
+//two strings are said to be the same if they are of the same length and same
+// char at each idx. 
+
+// Give two strings determine if the two are equal. '#' reps backspace 
+
+function compareStrings(s1, s2) {
+  let array1 = backspaced(s1)
+  let array2 = backspaced(s2);
+
+  let obj = {};
+
+  array1.forEach(char => {
+      if(!obj[char]){
+          obj[char] = 1
+      } else {
+          obj[char] += 1
+      }
+  })
+
+    array2.forEach((char) => {
+      if (!obj[char]) {
+        obj[char] = 1;
+      } else {
+        obj[char] -= 1;
+      }
+    });
+
+    if (Object.values(obj).every((el) => el === 0)){
+      return 1;
+    } else {
+      return 0;
+    }
+  
+}
+
+function backspaced(string) {
+  let array = [];
+  for (let i = 0; i < string.length; i++) {
+    if ((string[i] !== "#")) {
+      array.push(string[i]);
+    } else {
+      array.pop()
+    }
+  }
+
+  return array
+}
+
+
+
 // a 2-D grid consistening of some blocked represented at '#' and some unblocked
 // represented as '.' cell is given. The starting position ofa  pinter is in the top-left corner of the grid 
 // it is gauranteerd that the startign position is an unblocked cell. 
@@ -22,8 +72,6 @@ let grid = [
 // it will take the pointer 5 seconds to reach the bottom right as long as k >= 5
 // return Yes. 
 
-
-
 function reachTheEnd(grid, maxTime){
     if(grid.length === 0) return 0;
     let totalRow = grid.length;
@@ -34,15 +82,11 @@ function reachTheEnd(grid, maxTime){
         grid[i] = grid[i].split('')
     }
 
-    // console.log(grid);
-
     for(let i = 0; i < totalRow; i++){
         for(let j = 0; j < totalCol; j++){
             if(grid[i][j] === '.'){
                 count += 1; 
                 grid[i][j] = '#'
-                // console.log(grid);
-                // console.log(count);
                 if((i === totalRow-1) && (j === totalCol-1)){
                     console.log('last one');
                     if (count <= maxTime) {
@@ -101,5 +145,70 @@ function isPeriod(grid, row, col, totalRow, totalCol){
 }
 
 
+// console.log(reachTheEnd(grid, 5));
 
-console.log(reachTheEnd(grid, 5));
+
+// Given a collection of time intervals, [start, end], merge and return the over
+// lapping intervals sroted in ascending order of start time. 
+
+// return in ascending order 
+
+// let intervals = [
+//   [7, 7],
+//   [2, 3],
+//   [6, 11],
+//   [1, 2],
+// ];
+
+//Example 1:
+
+// let intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+// Example 2:
+
+// let intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+
+function getMergedIntervals(intervals) {
+  let sortedArray = intervals.sort((a, b) => a[1] - b[1]);
+  let finalArray = [];
+
+  for (let i = 0; i < sortedArray.length-1; i++) {
+    let interval = sortedArray[i]; 
+    let nextInterval = sortedArray[i + 1]
+
+    if(interval[0] < nextInterval[0] && interval[1] < nextInterval[0]) {
+        finalArray.push(interval, nextInterval)
+    } else {
+        finalArray.push(minmax(interval, nextInterval))
+        i+=1
+    }
+  }
+
+  return finalArray;
+}
+
+function minmax(arr1, arr2) {
+    let newArr = arr1.concat(arr2)
+
+    let min = Math.min(...newArr)
+    let max = Math.max(...newArr);
+
+    let final = []; 
+    final.push(min)
+    final.push(max)
+
+    return final
+}
+
+// console.log(minmax([1, 3], [2,6]));
+
+console.log(getMergedIntervals(intervals));
+
+
+
+
+
