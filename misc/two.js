acf44bf25ada1daf2a8a36d8df926b1734786bfb
ishafran <isabelle.shafran@gmail.com>
2021-02-17 20:45:02 -0800

    algos

--- misc/two.js
+++ misc/two.js
@@ -236,7 +236,7 @@ function valuesInBoth(array1, array2){
     return final; 
 }
 
-console.log(valuesInBoth([3, 13, 5, 6, 12], [12, 1, 33, 5]));
+// console.log(valuesInBoth([3, 13, 5, 6, 12], [12, 1, 33, 5]));
 
 // sorted 
 
@@ -477,3 +477,104 @@ function compress(string){
 // console.log(compress("aabbbbc"));
 
 // aabbbbc => a2b4c1;
+
+
+// Given a 2D integer array board representing the grid of candy, 
+// different positive integers board[i][j] represent different types of candies. 
+// A value of board[i][j] = 0 represents that the cell at position (i, j) is empty. 
+// The given board represents the state of the game following the player's move. 
+// Now, you need to restore the board to a stable state by crushing candies according to 
+
+// the following rules:
+// If three or more candies of the same type 
+// are adjacent vertically or horizontally, "crush" them all at the same time - 
+// these positions become empty.
+// After crushing all candies simultaneously, 
+// if an empty space on the board has candies on top of itself, 
+// then these candies will drop until they hit a candy or bottom at the same time.
+//  (No new candies will drop outside the top boundary.)
+// After the above steps, there may exist more candies that can be crushed. 
+// If so, you need to repeat the above steps.
+// If there does not exist more candies that can be crushed (ie. the board is stable), 
+// then return the current board.
+// You need to perform the above rules until the 
+// board becomes stable, then return the current board.
+ 
+// Example:
+// Input:
+let board = [
+  [110, 5, 112, 113, 114],
+  [210, 211, 5, 213, 214],
+  [310, 311, 3, 313, 314],
+  [410, 411, 412, 5, 414],
+    [5, 1, 512, 3, 3],
+  [610, 4, 1, 613, 614],
+  [710, 1, 2, 713, 714],
+  [810, 1, 2, 1, 1],
+    [1, 1, 2, 2, 2],
+  [4, 1, 4, 4, 1014],
+];
+    
+// Output:
+// [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[110,0,0,0,114],
+// [210,0,0,0,214],[310,0,0,113,314],[410,0,0,213,414],
+// [610,211,112,313,614],[710,311,412,613,714],[810,411,512,713,1014]]
+
+function candyCrush(board){
+    if(!board)return board;
+
+    let done = true; 
+
+    // TAG ROWS 
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[i].length - 2; j++){
+            let num1 = Math.abs(board[i][j]);
+            let num2 = Math.abs(board[i][j+1]);
+            let num3 = Math.abs(board[i][j+2])
+
+            if((num1 === num2 && num2 === num3) && num1 !== 0){
+                board[i][j] = -num1;
+                board[i][j+1] = -num2;
+                board[i][j+2] = -num3;
+                done = false
+            }
+        }
+    }
+
+    // TAG COLS 
+    for(let i = 0; i < board[0].length; i++){
+        for(let j = 0; j < board.length-2; j++){
+            let num1 = Math.abs(board[i][j]);
+            let num2 = Math.abs(board[i+1][j]);
+            let num3 = Math.abs(board[i+2][j]);
+
+            if (num1 === num2 && num2 === num3 && num1 !== 0) {
+              board[i][j] = -num1;
+              board[i+1][j] = -num2;
+              board[i+2][j] = -num3;
+              done = false;
+            }
+        }
+    }
+
+    // GRAVITY 
+
+    for(let j = 0; j < board[0].length; j++){
+
+        let idx = board.length - 1;
+
+        for(let i = board.length-1; i >= 0; i--){
+            if(board[j][i] > 0){
+                board[idx--][j] = board[i][j]
+            }
+        }
+
+        while(idx >= 0){
+            board[idx--][j] = 0;
+        }
+    }
+
+    return done ? candyCrush(board) : board;
+}
+
+console.log(candyCrush(board));
