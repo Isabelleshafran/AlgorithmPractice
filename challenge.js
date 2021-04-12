1214501ac84da05f69c21f82c9ccb835edd1bebb
ishafran <isabelle.shafran@gmail.com>
2021-04-12 11:22:25 -0400

    algo practice

--- challenge.js
+++ challenge.js
@@ -78,3 +78,30 @@ let board = [
   [4, 5, 6],
   [7, 8, 9],
 ];
+
+const isRobotBounded = function (instructions) {
+  let dir = 0,
+    x = 0,
+    y = 0;
+  for (let i of instructions) {
+    if (i == "L") dir++;
+    else if (i == "R") dir += 3;
+    else {
+      switch (dir % 4) {
+        case 0:
+          y++;
+          break;
+        case 1:
+          x++;
+          break;
+        case 2:
+          y--;
+          break;
+        case 3:
+          x--;
+          break;
+      }
+    }
+  }
+  return dir % 4 != 0 || (!x && !y);
+};
